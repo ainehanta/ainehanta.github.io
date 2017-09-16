db208b46e86f6a09f6078c9de2afdde72c599224
Makoto Uju <ainehanta@hotmail.co.jp>
2017-09-16 22:12:12 +0900

    Add media query

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -7,9 +7,22 @@ import 'vue-awesome/icons/github'
 import 'vue-awesome/icons/key'
 
 import { Carousel, Slide } from 'vue-carousel'
+import { MediaQueries, CommonBands } from 'vue-media-queries'
+
+const mediaQueries = new MediaQueries({
+  bands: CommonBands.Bulma
+})
+
+Vue.use(mediaQueries)
 
 Vue.component('icon', Icon)
 Vue.component('carousel', Carousel)
 Vue.component('slide', Slide)
 
 window.Vue = Vue
+
+new Vue({
+  el: '#content',
+  mediaQueries: mediaQueries,
+  mixins: [CommonBands.Bulma.mixin]
+});
